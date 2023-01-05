74f9b1446170acd87c456b0532e2c47a60bea396
Rob Errera <roberrera@Robs-MacBook-Pro.local>
2023-01-04 22:11:58 -0500

    Add Row, Image, and Column composables

--- app/src/main/java/com/roberrera/composetutorial/MainActivity.kt
+++ app/src/main/java/com/roberrera/composetutorial/MainActivity.kt
@@ -3,12 +3,16 @@ package com.roberrera.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.roberrera.composetutorial.ui.theme.ComposeTutorialTheme
 
@@ -16,28 +20,31 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTutorialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(message: Message) {
+    Row {
+        Image(
+            painter = painterResource(id = R.drawable.gbc),
+            contentDescription = "Contact profile picture"
+        )
+    }
+    Column {
+        Text(text = message.author)
+        Text(text = message.body)
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    ComposeTutorialTheme {
-        Greeting("Android")
-    }
+fun PreviewMessageCard() {
+    MessageCard(
+        message = Message("Colleague", "Hey, take a look at this.")
+    )
 }
