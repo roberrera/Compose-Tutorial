e04dfeaaaa38e61067426dcc21b9aa1cdb924228
Rob Errera <roberrera@Robs-MacBook-Pro.local>
2023-01-05 18:13:54 -0500

    Customize layout dimensions and reduce hardcoding

--- app/src/main/java/com/roberrera/composetutorial/MainActivity.kt
+++ app/src/main/java/com/roberrera/composetutorial/MainActivity.kt
@@ -4,16 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.roberrera.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,15 +30,21 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(message: Message) {
-    Row {
+    // Add padding
+    Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(id = R.drawable.gbc),
-            contentDescription = "Contact profile picture"
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
         )
-    }
-    Column {
-        Text(text = message.author)
-        Text(text = message.body)
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(text = message.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = message.body)
+        }
     }
 }
 
